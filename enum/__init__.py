7dcfeebd094cedd604012fa9d01074de3aedf782
cielavenir <cielartisan@gmail.com>
2021-11-19 15:52:53 +0900

    add enum.py

---
+++ enum/__init__.py
@@ -0,0 +1,3 @@
+import aenum
+__all__ = aenum.__all__
+from aenum import *
---
+++ setup.py
@@ -0,0 +1,31 @@
+
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='enum34',
+    version="2.0.0",
+    author='cielavenir',
+    author_email='cielartisan@gmail.com',
+    description='fake enum34 redirecting to aenum',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/cielavenir/enum34-aenum-proxy',
+    keywords='enum enum34 aenum',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development',
+    ],
+    packages=setuptools.find_packages(),
+    python_requires=">=2.7",
+    install_requires=[
+        'aenum',
+    ],
+)
+
