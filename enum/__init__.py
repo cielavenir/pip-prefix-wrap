520f6e63e88f33738224383ea2c56114c93801c8
cielavenir <cielartisan@gmail.com>
2021-11-19 16:44:03 +0900

    Fixed import on Python 3.5+

--- enum/__init__.py
+++ enum/__init__.py
@@ -1,3 +1,19 @@
-import aenum
-__all__ = aenum.__all__
-from aenum import *
+import sys
+if sys.version_info>=(3,5):
+    # if somehow loaded on Python 3.5+ (possibly by buggy PYTHONPATH), should load original enum
+    # using technique described in https://qiita.com/cielavenir/items/8d3ca1b6a0b6298b4d55
+    from os.path import dirname
+    from os.path import realpath
+    from importlib import reload
+    myroot = realpath(dirname(dirname(__file__)))
+    for i in range(len(sys.path)-1,-1,-1):
+        if realpath(sys.path[i]) == myroot:
+            sys.path.pop(i)
+    import enum
+    reload(enum)
+    __all__ = enum.__all__
+    from enum import *
+else:
+    import aenum
+    __all__ = aenum.__all__
+    from aenum import *
